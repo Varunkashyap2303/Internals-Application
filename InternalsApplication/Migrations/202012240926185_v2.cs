5f056ad0db76d905686bdab7b590b4dc23a6c11a
Varunkashyap2303 <r.varunkashyap99@gmail.com>
2020-12-26 10:41:47 +0530

    Fixed view of the pages

--- InternalsApplication/Migrations/202012240926185_v2.cs
+++ InternalsApplication/Migrations/202012240926185_v2.cs
@@ -3,20 +3,20 @@ namespace InternalsApplication.Migrations
     using System;
     using System.Data.Entity.Migrations;
     
-    public partial class v1 : DbMigration
+    public partial class v2 : DbMigration
     {
         public override void Up()
         {
             CreateTable(
                 "dbo.ClassRoom",
                 c => new
-                    {
-                        Room_No = c.Int(nullable: false),
-                        Duration = c.Time(precision: 7),
-                        Dept_No = c.Int(),
-                        Teacher_ID = c.String(maxLength: 255),
-                        USN = c.String(maxLength: 255),
-                    })
+                {
+                    Room_No = c.Int(nullable: false),
+                    Duration = c.Time(precision: 7),
+                    Dept_No = c.Int(),
+                    Teacher_ID = c.String(maxLength: 255),
+                    USN = c.String(maxLength: 255),
+                })
                 .PrimaryKey(t => t.Room_No)
                 .ForeignKey("dbo.Department", t => t.Dept_No)
                 .ForeignKey("dbo.Teacher", t => t.Teacher_ID)
@@ -24,29 +24,29 @@ namespace InternalsApplication.Migrations
                 .Index(t => t.Dept_No)
                 .Index(t => t.Teacher_ID)
                 .Index(t => t.USN);
-            
+
             CreateTable(
                 "dbo.Department",
                 c => new
-                    {
-                        Dept_No = c.Int(nullable: false),
-                        Dept_Name = c.String(maxLength: 255),
-                    })
+                {
+                    Dept_No = c.Int(nullable: false),
+                    Dept_Name = c.String(maxLength: 255),
+                })
                 .PrimaryKey(t => t.Dept_No);
-            
+
             CreateTable(
                 "dbo.Report",
                 c => new
-                    {
-                        Report_ID = c.Long(nullable: false),
-                        Total = c.Int(),
-                        Percentage = c.Decimal(precision: 5, scale: 3, storeType: "numeric"),
-                        USN = c.String(maxLength: 255),
-                        Teacher_ID = c.String(maxLength: 255),
-                        Dept_No = c.Int(),
-                        Subject_Code = c.String(maxLength: 255),
-                        Script_ID = c.Int(),
-                    })
+                {
+                    Report_ID = c.Long(nullable: false),
+                    Total = c.Int(),
+                    Percentage = c.Decimal(precision: 5, scale: 3, storeType: "numeric"),
+                    USN = c.String(maxLength: 255),
+                    Teacher_ID = c.String(maxLength: 255),
+                    Dept_No = c.Int(),
+                    Subject_Code = c.String(maxLength: 255),
+                    Script_ID = c.Int(),
+                })
                 .PrimaryKey(t => t.Report_ID)
                 .ForeignKey("dbo.Department", t => t.Dept_No)
                 .ForeignKey("dbo.Script", t => t.Script_ID)
@@ -58,20 +58,20 @@ namespace InternalsApplication.Migrations
                 .Index(t => t.Dept_No)
                 .Index(t => t.Subject_Code)
                 .Index(t => t.Script_ID);
-            
+
             CreateTable(
                 "dbo.Script",
                 c => new
-                    {
-                        Script_ID = c.Int(nullable: false),
-                        IA1 = c.Int(),
-                        IA2 = c.Int(),
-                        IA3 = c.Int(),
-                        Teacher_ID = c.String(maxLength: 255),
-                        Subject_Code = c.String(maxLength: 255),
-                        USN = c.String(maxLength: 255),
-                        Dept_No = c.Int(),
-                    })
+                {
+                    Script_ID = c.Int(nullable: false),
+                    IA1 = c.Int(),
+                    IA2 = c.Int(),
+                    IA3 = c.Int(),
+                    Teacher_ID = c.String(maxLength: 255),
+                    Subject_Code = c.String(maxLength: 255),
+                    USN = c.String(maxLength: 255),
+                    Dept_No = c.Int(),
+                })
                 .PrimaryKey(t => t.Script_ID)
                 .ForeignKey("dbo.Department", t => t.Dept_No)
                 .ForeignKey("dbo.Student", t => t.USN)
@@ -81,120 +81,119 @@ namespace InternalsApplication.Migrations
                 .Index(t => t.Subject_Code)
                 .Index(t => t.USN)
                 .Index(t => t.Dept_No);
-            
+
             CreateTable(
                 "dbo.Student",
                 c => new
-                    {
-                        USN = c.String(nullable: false, maxLength: 255),
-                        Name = c.String(maxLength: 255),
-                        Semester = c.Int(),
-                        Dept_ID = c.Int(),
-                    })
+                {
+                    USN = c.String(nullable: false, maxLength: 255),
+                    Name = c.String(maxLength: 255),
+                    Semester = c.Int(),
+                    Dept_No = c.Int(),
+                })
                 .PrimaryKey(t => t.USN)
-                .ForeignKey("dbo.Department", t => t.Dept_ID);
-            
+                .ForeignKey("dbo.Department", t => t.Dept_No);
+
             CreateTable(
                 "dbo.Subject",
                 c => new
-                    {
-                        Subject_Code = c.String(nullable: false, maxLength: 255),
-                        Subject_Name = c.String(maxLength: 255),
-                    })
+                {
+                    Subject_Code = c.String(nullable: false, maxLength: 255),
+                    Subject_Name = c.String(maxLength: 255),
+                })
                 .PrimaryKey(t => t.Subject_Code);
-            
+
             CreateTable(
                 "dbo.Teacher",
                 c => new
-                    {
-                        Teacher_ID = c.String(nullable: false, maxLength: 255),
-                        Teacher_Name = c.String(maxLength: 255),
-                        Dept_No = c.Int(),
-                    })
+                {
+                    Teacher_ID = c.String(nullable: false, maxLength: 255),
+                    Teacher_Name = c.String(maxLength: 255),
+                    Dept_No = c.Int(),
+                })
                 .PrimaryKey(t => t.Teacher_ID)
                 .ForeignKey("dbo.Department", t => t.Dept_No)
                 .Index(t => t.Dept_No);
-            
+
             CreateTable(
                 "dbo.AspNetRoles",
                 c => new
-                    {
-                        Id = c.String(nullable: false, maxLength: 128),
-                        Name = c.String(nullable: false, maxLength: 256),
-                    })
+                {
+                    Id = c.String(nullable: false, maxLength: 128),
+                    Name = c.String(nullable: false, maxLength: 256),
+                })
                 .PrimaryKey(t => t.Id)
                 .Index(t => t.Name, unique: true, name: "RoleNameIndex");
-            
+
             CreateTable(
                 "dbo.AspNetUserRoles",
                 c => new
-                    {
-                        UserId = c.String(nullable: false, maxLength: 128),
-                        RoleId = c.String(nullable: false, maxLength: 128),
-                    })
+                {
+                    UserId = c.String(nullable: false, maxLength: 128),
+                    RoleId = c.String(nullable: false, maxLength: 128),
+                })
                 .PrimaryKey(t => new { t.UserId, t.RoleId })
                 .ForeignKey("dbo.AspNetRoles", t => t.RoleId, cascadeDelete: true)
                 .ForeignKey("dbo.AspNetUsers", t => t.UserId, cascadeDelete: true)
                 .Index(t => t.UserId)
                 .Index(t => t.RoleId);
-            
+
             CreateTable(
                 "dbo.sysdiagrams",
                 c => new
-                    {
-                        diagram_id = c.Int(nullable: false, identity: true),
-                        name = c.String(nullable: false, maxLength: 128),
-                        principal_id = c.Int(nullable: false),
-                        version = c.Int(),
-                        definition = c.Binary(),
-                    })
+                {
+                    diagram_id = c.Int(nullable: false, identity: true),
+                    name = c.String(nullable: false, maxLength: 128),
+                    principal_id = c.Int(nullable: false),
+                    version = c.Int(),
+                    definition = c.Binary(),
+                })
                 .PrimaryKey(t => t.diagram_id);
-            
+
             CreateTable(
                 "dbo.AspNetUsers",
                 c => new
-                    {
-                        Id = c.String(nullable: false, maxLength: 128),
-                        Email = c.String(maxLength: 256),
-                        EmailConfirmed = c.Boolean(nullable: false),
-                        PasswordHash = c.String(),
-                        SecurityStamp = c.String(),
-                        PhoneNumber = c.String(),
-                        PhoneNumberConfirmed = c.Boolean(nullable: false),
-                        TwoFactorEnabled = c.Boolean(nullable: false),
-                        LockoutEndDateUtc = c.DateTime(),
-                        LockoutEnabled = c.Boolean(nullable: false),
-                        AccessFailedCount = c.Int(nullable: false),
-                        UserName = c.String(nullable: false, maxLength: 256),
-                    })
+                {
+                    Id = c.String(nullable: false, maxLength: 128),
+                    Email = c.String(maxLength: 256),
+                    EmailConfirmed = c.Boolean(nullable: false),
+                    PasswordHash = c.String(),
+                    SecurityStamp = c.String(),
+                    PhoneNumber = c.String(),
+                    PhoneNumberConfirmed = c.Boolean(nullable: false),
+                    TwoFactorEnabled = c.Boolean(nullable: false),
+                    LockoutEndDateUtc = c.DateTime(),
+                    LockoutEnabled = c.Boolean(nullable: false),
+                    AccessFailedCount = c.Int(nullable: false),
+                    UserName = c.String(nullable: false, maxLength: 256),
+                })
                 .PrimaryKey(t => t.Id)
                 .Index(t => t.UserName, unique: true, name: "UserNameIndex");
-            
+
             CreateTable(
                 "dbo.AspNetUserClaims",
                 c => new
-                    {
-                        Id = c.Int(nullable: false, identity: true),
-                        UserId = c.String(nullable: false, maxLength: 128),
-                        ClaimType = c.String(),
-                        ClaimValue = c.String(),
-                    })
+                {
+                    Id = c.Int(nullable: false, identity: true),
+                    UserId = c.String(nullable: false, maxLength: 128),
+                    ClaimType = c.String(),
+                    ClaimValue = c.String(),
+                })
                 .PrimaryKey(t => t.Id)
                 .ForeignKey("dbo.AspNetUsers", t => t.UserId, cascadeDelete: true)
                 .Index(t => t.UserId);
-            
+
             CreateTable(
                 "dbo.AspNetUserLogins",
                 c => new
-                    {
-                        LoginProvider = c.String(nullable: false, maxLength: 128),
-                        ProviderKey = c.String(nullable: false, maxLength: 128),
-                        UserId = c.String(nullable: false, maxLength: 128),
-                    })
+                {
+                    LoginProvider = c.String(nullable: false, maxLength: 128),
+                    ProviderKey = c.String(nullable: false, maxLength: 128),
+                    UserId = c.String(nullable: false, maxLength: 128),
+                })
                 .PrimaryKey(t => new { t.LoginProvider, t.ProviderKey, t.UserId })
                 .ForeignKey("dbo.AspNetUsers", t => t.UserId, cascadeDelete: true)
                 .Index(t => t.UserId);
-            
         }
         
         public override void Down()
